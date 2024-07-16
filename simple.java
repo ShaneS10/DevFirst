fb7df91aa4ae7a1742ab245fc2060d21484a21f7
SHANE <shanemathew2003@gmail.com>
2024-07-16 11:02:55 +0530

    F1

--- simple.java
+++ simple.java
@@ -2,6 +2,6 @@ public class simple
 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Changes");
+        System.out.println("No");
     }
 }
